7caefe614fa94df86a450be24b53ad7a8d27628b
Lunn <thafm.work@gmail.com>
2024-11-27 23:16:32 +0000

    Update SideBar; Add shop path

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -12,6 +12,7 @@ import ChallengeIcon from "@mui/icons-material/EmojiEvents";
 import GratitudeIcon from "@mui/icons-material/Favorite";
 import MeditationIcon from "@mui/icons-material/SelfImprovement";
 import JournalIcon from "@mui/icons-material/Book";
+import StorefrontIcon from '@mui/icons-material/Storefront';
 
 function SideBar() {
   const [isMounted, setIsMounted] = useState(false);
@@ -24,18 +25,15 @@ function SideBar() {
 
   return (
     <aside className="fixed left-0 top-0 w-16 h-screen bg-gray-800 flex flex-col items-center py-6 transition-all duration-300 hover:w-24 md:w-20 md:hover:w-24 shadow-xl">
-      <Link href="/" className="mb-8 flex flex-col items-center text-white">
-        <HouseboatIcon className="text-4xl mb-1" />
-        <span className="text-xs hidden md:block">Home</span>
-      </Link>
-
       <nav className="flex flex-col space-y-6 text-white">
+        <NavLink href="/" icon={<HouseboatIcon  />} label="Home" />
         <NavLink href="/create-task" icon={<TaskIcon />} label="Add Tasks" />
         <NavLink href="/user-tasks" icon={<AssignmentIcon />} label="Your Tasks" />
         <NavLink href="/daily-challenge" icon={<ChallengeIcon />} label="Daily Challenge" />
         <NavLink href="/gratitude-log" icon={<GratitudeIcon />} label="Gratitude" />
         <NavLink href="/meditation" icon={<MeditationIcon />} label="Meditation" />
         <NavLink href="/journal" icon={<JournalIcon />} label="Journal" />
+        <NavLink href="/shop" icon={<StorefrontIcon />} label="Shop" />
       </nav>
     </aside>
   );
